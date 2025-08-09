83c1f91ab500be3c4e10a529945f29d1552fe2d6
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-08-09 15:47:10 +0900

    Initial commit

---
+++ src/main/java/com/taja/TajaApplication.java
@@ -0,0 +1,13 @@
+package com.taja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TajaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TajaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taja/TajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TajaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
