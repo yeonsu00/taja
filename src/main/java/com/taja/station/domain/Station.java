97008b844fa8e2919c21bd3704f2a5524e3c266d
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-08-27 23:36:30 +0900

    feat: Station - 자치구, lcd 거치대수, qr 거치대수, 운영방식 추가

--- src/main/java/com/taja/station/domain/Station.java
+++ src/main/java/com/taja/station/domain/Station.java
@@ -10,19 +10,28 @@ public class Station {
 
     private String number;
 
+    private String district;
+
     private String address;
 
     private double latitude;
 
     private double longitude;
 
+    private int lcd;
+
+    private int qr;
+
     @Builder
-    public Station(String name, String number, String address, double latitude, double longitude) {
+    public Station(String name, String number, String district, String address, double latitude, double longitude, int lcd, int qr) {
         this.name = name;
         this.number = number;
+        this.district = district;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.lcd = lcd;
+        this.qr = qr;
     }
 
 }
--- src/main/java/com/taja/station/infra/StationEntity.java
+++ src/main/java/com/taja/station/infra/StationEntity.java
@@ -22,28 +22,54 @@ public class StationEntity {
 
     private String number;
 
+    private String district;
+
     private String address;
 
     private double latitude;
 
     private double longitude;
 
+    private int lcd;
+
+    private int qr;
+
+    private String operationMethod;
+
     @Builder
-    public StationEntity(String name, String number, String address, double latitude, double longitude) {
+    public StationEntity(String name, String number, String district, String address, double latitude, double longitude, int lcd, int qr) {
         this.name = name;
         this.number = number;
+        this.district = district;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
+        this.lcd = lcd;
+        this.qr = qr;
+        this.operationMethod = getOperationMethod(lcd, qr);
+    }
+
+    private String getOperationMethod(int lcd, int qr) {
+        if (lcd > 0 && qr > 0) {
+            return "LCD/QR";
+        } else if (lcd > 0) {
+            return "LCD";
+        } else if (qr > 0) {
+            return "QR";
+        }
+        return "None";
     }
 
     public static StationEntity fromStation(Station station) {
         return StationEntity.builder()
                 .name(station.getName())
                 .number(station.getNumber())
+                .district(station.getDistrict())
                 .address(station.getAddress())
                 .latitude(station.getLatitude())
                 .longitude(station.getLongitude())
+                .lcd(station.getLcd())
+                .qr(station.getQr())
                 .build();
     }
 }
