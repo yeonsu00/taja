f0084eda546a39398bc9ae456e879c43450899cc
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-08-31 17:03:30 +0900

    feat: Station Status domain, entity 추가

---
+++ src/main/java/com/taja/collector/domain/StationStatus.java
@@ -0,0 +1,29 @@
+package com.taja.collector.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class StationStatus {
+
+    private Long stationStatusId;
+
+    private Integer stationNumber;
+
+    private String stationName;
+
+    private Integer parkingBikeTotalCount;
+
+    private LocalDateTime requestedAt;
+
+    @Builder
+    public StationStatus(Long stationStatusId, Integer stationNumber, String stationName, Integer parkingBikeTotalCount,
+                         LocalDateTime requestedAt) {
+        this.stationStatusId = stationStatusId;
+        this.stationNumber = stationNumber;
+        this.stationName = stationName;
+        this.parkingBikeTotalCount = parkingBikeTotalCount;
+        this.requestedAt = requestedAt;
+    }
+}
