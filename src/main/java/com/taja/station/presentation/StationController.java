dfa4935f8eedfd5f417eeaf560f81aed68be55dc
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-09-09 15:35:10 +0900

    feat: 근처 대여소 조회 요청 파라미터로 변경

--- src/main/java/com/taja/station/presentation/StationController.java
+++ src/main/java/com/taja/station/presentation/StationController.java
@@ -9,8 +9,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,7 +31,7 @@ public class StationController {
     }
 
     @GetMapping("/nearby")
-    public CommonApiResponse<List<NearbyStationResponse>> findNearbyStations(@Valid @RequestBody NearbyStationRequest nearbyStationRequest) {
+    public CommonApiResponse<List<NearbyStationResponse>> findNearbyStations(@Valid @ModelAttribute NearbyStationRequest nearbyStationRequest) {
         List<NearbyStationResponse> nearbyStations = stationService.findNearbyStations(
                 nearbyStationRequest.latitude(),
                 nearbyStationRequest.longitude(),
