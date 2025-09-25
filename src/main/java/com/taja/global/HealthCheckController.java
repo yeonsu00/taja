53d04bd8497ee7dced8e49eb5d9b3f3dc3e8a113
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-09-25 15:57:57 +0900

    refactor: health check에서 디버깅 구문 삭제

--- src/main/java/com/taja/global/HealthCheckController.java
+++ src/main/java/com/taja/global/HealthCheckController.java
@@ -1,16 +1,18 @@
 package com.taja.global;
 
 import com.taja.jwt.CustomUserDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HealthCheckController {
 
     @GetMapping("/health")
     public String healthCheck(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
-        System.out.println("test" + customUserDetails.getUsername());
+        log.info("test {}", customUserDetails.getUsername());
         return "OK";
     }
 }
