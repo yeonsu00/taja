8791e4b2dc9a94942df4720fa4b5e91c6dc1902f
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-10-14 20:57:03 +0900

    feat: Controller에 Swagger 어노테이션 적용

--- src/main/java/com/taja/global/HealthCheckController.java
+++ src/main/java/com/taja/global/HealthCheckController.java
@@ -1,6 +1,8 @@
 package com.taja.global;
 
 import com.taja.jwt.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@Tag(name = "healthcheck", description = "healthcheck API")
 public class HealthCheckController {
 
+    @Operation(summary = "health check", description = "서버 상태 확인")
     @GetMapping("/health")
     public String healthCheck(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
         log.info("test {}", customUserDetails.getUsername());
