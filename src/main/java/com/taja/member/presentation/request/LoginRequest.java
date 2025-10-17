d125e49d3c8afe2e853df0fd0ba9188118105e5e
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-10-17 17:31:28 +0900

    refactor: 로그인 응답 메시지 수정

--- src/main/java/com/taja/member/presentation/request/LoginRequest.java
+++ src/main/java/com/taja/member/presentation/request/LoginRequest.java
@@ -6,11 +6,11 @@ import jakarta.validation.constraints.Size;
 
 public record LoginRequest(
         @NotBlank(message = "이메일은 필수입니다.")
-        @Email(message = "유효한 이메일 형식이어야 합니다.")
+        @Email(message = "올바른 이메일 형식이 아닙니다.")
         String email,
 
         @NotBlank(message = "비밀번호는 필수입니다.")
-        @Size(min = 4, max = 20, message = "비밀번호는 4자 이상 20자 이하이어야 합니다.")
+        @Size(min = 4, max = 20, message = "올바른 비밀번호 형식이 아닙니다. (4자 이상 20자 이하)")
         String password
 ) {
 }
