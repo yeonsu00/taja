53b2f3f48c9444a37672e37c99a505c93d99f5e0
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2025-11-19 17:06:53 +0900

    feat: 요청일자 필드에 대한 유효성 검사 및 JSON 포맷 추가

--- src/main/java/com/taja/statistics/presentation/request/StatisticsRequest.java
+++ src/main/java/com/taja/statistics/presentation/request/StatisticsRequest.java
@@ -1,10 +1,12 @@
 package com.taja.statistics.presentation.request;
 
-import jakarta.validation.constraints.NotBlank;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record StatisticsRequest(
-        @NotBlank(message = "요청일자는 필수 입력값입니다.")
+        @NotNull(message = "요청일자는 필수 입력값입니다.")
+        @JsonFormat(pattern = "yyyy-MM-dd")
         LocalDate requestedAt
 ) {
 }
