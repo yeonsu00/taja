930633581470ee6a7f57394ba5fb40c0e03b3c50
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2026-01-24 02:05:05 +0900

    test: SeoulDataStationStatusClient 테스트 추가

--- src/test/java/com/taja/infrastructure/client/bike/SeoulDataStationInfoClientTest.java
+++ src/test/java/com/taja/infrastructure/client/bike/SeoulDataStationInfoClientTest.java
@@ -1,7 +1,6 @@
 package com.taja.infrastructure.client.bike;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
